32abed1f5fcd2e7f930dc701cbcf50b10122588e
Ankit Verma <av.ankitverma@yahoo.com>
2022-08-21 22:56:52 +0530

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import mosquito from "./assets/mosquito.png";
+import { Modal, Button } from "react-bootstrap";
+import "./App.css";
+import Game from "./components/game";
 
 function App() {
+  const [showStartModal, setshowStartModal] = useState(true);
+  const [startGame, setstartGame] = useState(false);
+  const handleClose = () => {
+    setshowStartModal(false);
+    setstartGame(true);
+  };
+
+  const stopGame = () => {
+    setstartGame(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {showStartModal ? (
+        <Modal
+          show={showStartModal}
+          onHide={() => {
+            setshowStartModal(false);
+          }}
+          backdrop="static"
+          keyboard={false}
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
         >
-          Learn React
-        </a>
-      </header>
+          <Modal.Header>
+            <Modal.Title>
+              Hit me If You Can!!{" "}
+              <img height="50em" src={mosquito} alt="mosquito" />
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Hi, <br />
+            Welcome to the Game! <br /> <br />
+            Hit the Mosquito to kill it and score points.
+            <br />
+            The game ends in 30 seconds automatically.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success" onClick={handleClose}>
+              Start Game
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ) : (
+        <Game startGame={startGame} stopGame={stopGame} />
+      )}
     </div>
   );
 }
---
+++ src/components/game.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import mosquito from "./../assets/mosquito.png";
+import mosquitoScared from "./../assets/mosquito-dead.png";
+import { Button, Modal } from "react-bootstrap";
+
+function Game() {
+  //   relaunch modal
+  const [showCloseModal, setshowCloseModal] = useState(false);
+
+  //   showScared mosquito
+  const [showHit, setshowHit] = useState(false);
+
+  //   position of mosquito
+  const [top, settop] = useState(Math.floor(Math.random() * 400));
+  const [left, setleft] = useState(Math.floor(Math.random() * 400));
+
+  //   score
+  const [count, setcount] = useState(0);
+
+  // useEffect as ComponentDidMount
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  //   game logic
+  const startGame = () => {
+    let time = setInterval(() => {
+      let topValue = Math.floor(Math.random() * 400);
+      let leftValue = Math.floor(Math.random() * 400);
+      settop(topValue);
+      setleft(leftValue);
+      setshowHit(false);
+    }, 1000);
+
+    setTimeout(() => {
+      clearTimeout(time);
+      setshowCloseModal(true);
+    }, 30000);
+  };
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+  return (
+    <div className="App">
+      <div className="score">Score : {count}</div>
+      <div className="App-header">
+        {showHit ? (
+          <img
+            src={mosquitoScared}
+            className="mosquito"
+            style={{
+              position: "absolute",
+              top: `${top}px`,
+              left: `${left}px`,
+            }}
+            alt="mosquito"
+          />
+        ) : (
+          <img
+            onClick={() => {
+              setcount(count + 1);
+              setshowHit(true);
+            }}
+            src={mosquito}
+            className="mosquito"
+            style={{
+              position: "absolute",
+              top: `${top}px`,
+              left: `${left}px`,
+            }}
+            alt="mosquito"
+          />
+        )}
+        {/* Close Modal */}
+        <div>
+          <Modal
+            show={showCloseModal}
+            backdrop="static"
+            keyboard={false}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header>
+              <Modal.Title>
+                Game Over!! <img height="50em" src={mosquito} alt="mosquito" />
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Your Score: {count}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="warning" onClick={refreshPage}>
+                relaunch
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
